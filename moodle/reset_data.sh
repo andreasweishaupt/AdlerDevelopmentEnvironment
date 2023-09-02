dfb8fd44f1b9b1329588d7a0801399cfd90dbbe9
Glutamat42 <markus.heck13396@gmx.de>
2023-09-02 17:27:35 +0200

    refactor to use docker-compose, include phpmyadmin, fix generated moodle config file, update README.md

--- moodle/reset_data.sh
+++ moodle/reset_data.sh
@@ -3,4 +3,4 @@ MOODLE_PARENT_DIRECTORY=/home/markus
 
 sudo rm -r $MOODLE_PARENT_DIRECTORY/moodledata $MOODLE_PARENT_DIRECTORY/moodledata_phpu
 sudo rm $MOODLE_PARENT_DIRECTORY/moodle/config.php
-sudo docker stop moodle_mariadb && docker rm moodle_mariadb
+sudo docker compose down -v
