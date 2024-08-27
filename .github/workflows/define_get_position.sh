49a9da26df74e278f06922511ca97d6a3ae9dd55
Andreas Weishaupt <31248215+andreasweishaupt@users.noreply.github.com>
2024-08-27 19:47:46 +0200

    Update define_get_position.sh

--- .github/workflows/define_get_position.sh
+++ .github/workflows/define_get_position.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 GET_POSITION() {
-  python "$GITHUB_WORKSPACE/.github/workflows/find_element.py" "$1" "$2" "360" "140"
+  python "$GITHUB_WORKSPACE/.github/workflows/find_element.py" "$1" "$2" 360 140
 }
 
 export -f GET_POSITION
