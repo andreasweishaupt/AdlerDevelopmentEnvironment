70ecc0e5b1c739fea8e2d61b3f64125a486a4831
Andreas Weishaupt <andreas-93@web.de>
2024-09-06 14:12:27 +0200

    Update setup.sh

--- moodle/setup.sh
+++ moodle/setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e -x
 WSL_USER=$(awk -F: '($3>=1000)&&($3!=65534){print $1, $3}' /etc/passwd | sort -k2 -n | tail -1 | cut -d' ' -f1)
 MOODLE_PARENT_DIRECTORY=$(getent passwd $WSL_USER | cut -d: -f6)
 HOST_IP=$(ip route | grep default | awk '{print $3}')
@@ -202,7 +203,7 @@ php admin/tool/behat/cli/init.php
 
 echo moodle login data: username: ${_MOODLE_USER} password: ${_MOODLE_PW}
 echo db root password: ${_DB_ROOT_PW}
-echo Host IP (for IDE config): $HOST_IP
+echo "Host IP (for IDE config): $HOST_IP"
 
 
 
