add8bc19849ccf9715a2942073d26e6a9540a9db
Andreas Weishaupt <andreas-93@web.de>
2024-09-17 12:35:13 +0200

    add imports

--- .github/workflows/browser_functions.py
+++ .github/workflows/browser_functions.py
@@ -1,11 +1,14 @@
 import sys
 import logging
+import json
+import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.remote.webdriver import WebDriver as RemoteWebDriver
 from selenium.common.exceptions import StaleElementReferenceException, TimeoutException
 import time
 
