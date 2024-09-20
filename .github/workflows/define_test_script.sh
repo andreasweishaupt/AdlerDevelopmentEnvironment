79c6a8dfd7b6452c667ab9f5ad618710fa9c5ad1
Andreas Weishaupt <andreas-93@web.de>
2024-09-20 14:59:13 +0200

    Update define_test_script.sh

--- .github/workflows/define_test_script.sh
+++ .github/workflows/define_test_script.sh
@@ -6,8 +6,8 @@ PYTHON_SCRIPT="$GITHUB_WORKSPACE/.github/workflows/test_script.py"
 # Funktion zum Finden eines Elements
 FIND_ELEMENT() {
     local coordinates=$(python "$PYTHON_SCRIPT" find "$1" "$2" | tail -n 1)
-	local coords
-    local IFS=',' read -ra coords <<< "$coordinates"
+    local coords
+    IFS=',' read -ra coords <<< "$coordinates"
     echo "${coords[@]}"
 }
 
