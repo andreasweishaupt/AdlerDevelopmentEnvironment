4cca40abaddb0092ff63f4a91f5fb680b7af984f
Andreas Weishaupt <andreas-93@web.de>
2024-09-20 14:42:48 +0200

    optimize

--- .github/workflows/define_test_script.sh
+++ .github/workflows/define_test_script.sh
@@ -5,7 +5,15 @@ PYTHON_SCRIPT="$GITHUB_WORKSPACE/.github/workflows/test_script.py"
 
 # Funktion zum Finden eines Elements
 FIND_ELEMENT() {
-    python "$PYTHON_SCRIPT" find "$1" "$2" "$3"
+	local identifier_type="$1"
+    local identifier="$2"
+    
+    # Ausgabe des Funktionsaufrufs
+    printf "FIND_ELEMENT(\"%s\",\"%s\")\n" "$identifier_type" "$identifier"
+    
+    local coordinates=$(python "$PYTHON_SCRIPT" find "$identifier_type" "$identifier" | tail -n 1)
+    local IFS=',' read -ra coords <<< "$coordinates"
+    echo "${coords[@]}"
 }
 
 export -f FIND_ELEMENT
