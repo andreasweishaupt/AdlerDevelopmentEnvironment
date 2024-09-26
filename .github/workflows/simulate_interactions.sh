125cc851d8f4739f02232f30bf22d12022133cb0
Andreas Weishaupt <andreas-93@web.de>
2024-09-26 15:03:05 +0200

    Update simulate_interactions.sh

--- .github/workflows/simulate_interactions.sh
+++ .github/workflows/simulate_interactions.sh
@@ -10,8 +10,7 @@ DISPLAY=:99 xdotool mousemove ${coords[0]} ${coords[1]} sleep 0.5 click 1 sleep 
 DISPLAY=:99 xdotool type "testWorld"
 DISPLAY=:99 xdotool sleep 1 key "Return"
 
-DISPLAY=:99 xdotool key ctrl+l sleep 1
-DISPLAY=:99 xdotool type "/runner/work"
+DISPLAY=:99 xdotool type "work"
 DISPLAY=:99 xdotool key Return
 sleep 2
 
