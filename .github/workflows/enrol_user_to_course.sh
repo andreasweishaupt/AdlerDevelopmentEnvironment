fd990c9387cba7adf8f9fec391fa325f5a5d75ff
Andreas Weishaupt <andreas-93@web.de>
2024-10-10 15:55:27 +0200

    next try

--- .github/workflows/enrol_user_to_course.sh
+++ .github/workflows/enrol_user_to_course.sh
@@ -59,6 +59,7 @@ options = Options()
 options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--disable-gpu")
 options.add_argument("window-size=1200,800")
 options.add_argument("--remote-debugging-port=9222")
 
