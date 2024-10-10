07ff592b3b152ec52589cf869990cdf4f5f60743
Andreas Weishaupt <andreas-93@web.de>
2024-10-10 14:34:38 +0200

    kill authoring tool before continue

--- .github/workflows/enrol_user_to_course.sh
+++ .github/workflows/enrol_user_to_course.sh
@@ -42,6 +42,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 
 # Shell variables
 moodleurl = "$moodleurl"
@@ -52,13 +54,16 @@ course_id = "$COURSE_ID"
 # Configuration
 TIMEOUT = 5  # seconds
 
-options = webdriver.ChromeOptions()
-options.add_argument('--headless')
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-gpu')
+service = Service('/usr/bin/chromedriver')
+options = Options()
+options.add_argument("--headless")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("window-size=1200,800")
+options.add_argument("--remote-debugging-port=9222")
 
 try:
-    driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome(service=service, options=options)
 
     # Login
     driver.get(f"{moodleurl}/login/index.php")
