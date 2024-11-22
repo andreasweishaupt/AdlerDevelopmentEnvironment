d8fe75fad59a18a79c4996165f0e20ac71d23c75
Andreas Weishaupt <31248215+andreasweishaupt@users.noreply.github.com>
2024-11-22 08:48:34 +0100

    Update simulate_interactions.sh

--- .github/workflows/scripts/simulate_interactions.sh
+++ .github/workflows/scripts/simulate_interactions.sh
@@ -80,7 +80,7 @@ DISPLAY=:99 xdotool mousemove ${coords[0]} ${coords[1]} sleep 0.1 click 1 sleep 
 
 echo Click on mud-input-root-outlined
 coords=($(FIND_ELEMENT "class" "mud-switch-button"))
-coords[1]=($(coords[1] - 70))
+coords[1]=($((coords[1] - 70)))
 DISPLAY=:99 xdotool mousemove ${coords[0]} ${coords[1]} sleep 0.1 click 1
 DISPLAY=:99 xdotool type "Macht es Spass, Pipelines zu erstellen?"
 sleep 0.1
@@ -91,18 +91,18 @@ DISPLAY=:99 xdotool mousemove ${coords[0]} ${coords[1]} sleep 0.1 click 1 sleep 
 
 echo first radio button
 coords=($(FIND_ELEMENT "type" "radio" 0))
-coords[0]=($(coords[0] + 60))
+coords[0]=($((coords[0] + 60)))
 DISPLAY=:99 xdotool mousemove ${coords[0]} ${coords[1]} sleep 0.1 click 1
 DISPLAY=:99 xdotool sleep 0.1 key "ctrl+a" key "BackSpace" sleep 0.1
 DISPLAY=:99 xdotool type "Ja"
 
 echo second radio button
 coords=($(FIND_ELEMENT "type" "radio" 1))
-coords[0]=($(coords[0] + 60))
+coords[0]=($((coords[0] + 60)))
 DISPLAY=:99 xdotool mousemove ${coords[0]} ${coords[1]} sleep 0.1 click 1
 DISPLAY=:99 xdotool sleep 0.1 key "ctrl+a" key "BackSpace" sleep 0.1
 DISPLAY=:99 xdotool type "Nein"
-coords[0]=($(coords[0] - 60))
+coords[0]=($((coords[0] - 60)))
 DISPLAY=:99 xdotool mousemove ${coords[0]} ${coords[1]} sleep 0.1 click 1 sleep 0.1
 
 echo Click on add answer
@@ -111,11 +111,11 @@ DISPLAY=:99 xdotool mousemove ${coords[0]} ${coords[1]} sleep 0.1 click 1 sleep 
 
 echo third radio button
 coords=($(FIND_ELEMENT "type" "radio" 2))
-coords[0]=($(coords[0] + 60))
+coords[0]=($((coords[0] + 60)))
 DISPLAY=:99 xdotool mousemove ${coords[0]} ${coords[1]} sleep 0.1 click 1
 DISPLAY=:99 xdotool sleep 0.1 key "ctrl+a" key "BackSpace" sleep 0.1
 DISPLAY=:99 xdotool type "Auf jeden Fall!!"
-coords[0]=($(coords[0] - 60))
+coords[0]=($((coords[0] - 60)))
 DISPLAY=:99 xdotool mousemove ${coords[0]} ${coords[1]} sleep 0.1 click 1 sleep 0.1
 
 echo Click on Erstellen
